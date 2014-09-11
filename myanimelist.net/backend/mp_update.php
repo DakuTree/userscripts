a4a52c4fcf596adadd7123d05131a3fd66169183
Daku <dakutree@codeanimu.net>
2014-09-11 05:08:23 +0100

    update backend

--- myanimelist.net/backend/mp_update.php
+++ myanimelist.net/backend/mp_update.php
@@ -4,16 +4,24 @@
 	header("Expires: Wed, 01 Jan 1995 12:00:00 GMT");
 
 	if(empty($_GET['db_id']) || empty($_GET['score_precise']) || empty($_GET['user_id']) || empty($_GET['type'])) die('Missing Parameter(s).');
-
 	if(!ctype_digit($_GET['db_id']) || !is_numeric($_GET['score_precise']) || !ctype_digit($_GET['user_id']) || !in_array($_GET['type'], array('anime', 'manga'))) die('Incorrect Parameter(s).');
-	list($dbid, $score, $userid, $type) = array((int) $_GET['db_id'], $_GET['score_precise'], (int) $_GET['user_id'], $_GET['type']);
+	list($dbid, $score, $userid, $type) = array((int) $_GET['db_id'], $_GET['score_precise'], (int) $_GET['user_id'], $_GET['type']."list");
 
 	include "config.php"; //Contains $dbhost / $dbuser / $dbpass / $dbname
-	$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); //Connect to MySQL Server
+	$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname); //Connect to MySQL Server
 
-	$sql = "INSERT INTO {$type}list (db_id, score_precise, user_id) VALUES ({$dbid}, {$score}, {$userid})
-			ON DUPLICATE KEY UPDATE score_precise = values(score_precise)";
-	$do = mysqli_query($link, $sql) or die(mysqli_error($link));
+	if($stmt = $mysqli->prepare("INSERT INTO $type (db_id, score_precise, user_id) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE score_precise = values(score_precise)")){
+		if(!$stmt->bind_param("idi", $dbid, $score, $userid)){
+			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+		if(!$stmt->execute()){
+			echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+		$stmt->close();
+	}else{
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
 
-	mysqli_close($link);
+
+	$mysqli->close();
 ?>
