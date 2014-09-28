cbcdcf046bcb1f767da730e44048653f65aeb35a
Daku <dakutree@codeanimu.net>
2014-09-28 21:45:13 +0100

    2.0.0 pre-release mal_history only thing missing is page which lists all this

--- myanimelist.net/backend/mh_update.php
+++ myanimelist.net/backend/mh_update.php
@@ -3,34 +3,35 @@
 	header("Cache-Control: max-age=0, no-cache, no-store, must-revalidate"); #Make sure file isn't cached
 	header("Expires: Wed, 01 Jan 1995 12:00:00 GMT");
 
-	foreach($_POST as $key => $field) { $_POST[$key] = ($field ?: NULL); }
+	foreach($_POST as $key => $field) { $_POST[$key] = ($field ?: NULL); } //Set undefined > NULL
 
-	include "config.php"; //Contains $dbhost / $dbuser / $dbpass / $dbname
-	$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname); //Connect to MySQL Server
+	if(empty($_POST['userid']) || empty($_POST['db_id']) || empty($_POST['type']) || empty($_POST['title']) || empty($_POST['status']) || empty($_POST['score']) || empty($_POST['addup'])) die('Missing Parameter(s).');
+	if(!ctype_digit($_POST['userid']) || !ctype_digit($_POST['db_id']) || !in_array($_POST['type'], array('anime', 'manga')) || !in_array($_POST['status'], array('Watching', 'Completed', 'On-Hold', 'Dropped', 'Plan to Watch', 'Reading', 'Plan to Read')) || !is_numeric($_POST['score']) || !in_array($_POST['addup'], array('Add', 'Update'))) die('Incorrect Parameter(s).');
+	list($userid, $db_id, $type, $title, $status, $score, $addup) = array((int) $_POST['userid'], (int) $_POST['db_id'], $_POST['type'], (string) $_POST['title'], $_POST['status'], $_POST['score'], $_POST['addup']);
 
-	if($_POST['type'] == 'anime'){
-		if($stmt = $mysqli->prepare("INSERT INTO anime (db_id, title, status, score, ep_watched, ep_count, addup) VALUES (?, ?, ?, ?, ?, ?, ?)")){
-			if(!$stmt->bind_param("isssiis", $_POST['db_id'], $_POST['title'], $_POST['status'], $_POST['score'], $_POST['ep_watched'], $_POST['ep_count'], $_POST['addup'])){
-				echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-			}
-			if(!$stmt->execute()){
-				echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-			}
+	include "config.php"; //Contains $dbhost / $dbuser / $dbpass / $mh_dbname
+	$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $mh_dbname); //Connect to MySQL Server
+
+	if($type == 'anime'){
+		if(empty($_POST['ep_watched']) || empty($_POST['ep_count'])) die('Missing Parameter(s).');
+		if(!ctype_digit($_POST['ep_watched']) || !ctype_digit($_POST['ep_count'])) die('Incorrect Parameter(s).');
+		if($stmt = $mysqli->prepare("INSERT INTO anime (userid, db_id, title, status, score, ep_watched, ep_count, addup) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")){
+			$stmt->bind_param("iisssiis", $userid, $db_id, $title, $status, $score, $_POST['ep_watched'], $_POST['ep_count'], $addup) or print("Binding parameters failed: ({$stmt->errno}) {$stmt->error}");
+			$stmt->execute() or print("Execute failed: (".$stmt->errno.") ".$stmt->error);
 			$stmt->close();
 		}else{
-		    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+		    echo "Prepare failed: (".$mysqli->errno.") ".$mysqli->error;
 		}
-	}elseif($_POST['type'] == 'manga'){
-		if($stmt = $mysqli->prepare("INSERT INTO manga (db_id, title, status, score, ch_read, ch_count, vol_read, vol_count, addup) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)")){
-			if(!$stmt->bind_param("isssiiiis", $_POST['db_id'], $_POST['title'], $_POST['status'], $_POST['score'], $_POST['ch_read'], $_POST['ch_count'], $_POST['vol_read'], $_POST['vol_count'], $_POST['addup'])){
-				echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-			}
-			if(!$stmt->execute()){
-				echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-			}
+	}
+	elseif($type == 'manga'){
+		if(empty($_POST['ch_read']) || empty($_POST['ch_count']) || empty($_POST['vol_read']) || empty($_POST['vol_count'])) die('Missing Parameter(s).');
+		if(!ctype_digit($_POST['ch_read']) || !ctype_digit($_POST['ch_count']) || !ctype_digit($_POST['vol_read']) || !ctype_digit($_POST['vol_count'])) die('Incorrect Parameter(s).');
+		if($stmt = $mysqli->prepare("INSERT INTO manga (userid, db_id, title, status, score, ch_read, ch_count, vol_read, vol_count, addup) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")){
+			$stmt->bind_param("iisssiiiis", $userid, $db_id, $title, $status, $score, $_POST['ch_read'], $_POST['ch_count'], $_POST['vol_read'], $_POST['vol_count'], $addup) or print("Binding parameters failed: ({$stmt->errno}) {$stmt->error}");
+			$stmt->execute() or print("Execute failed: (".$stmt->errno.") ".$stmt->error);
 			$stmt->close();
 		}else{
-		    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+		    echo "Prepare failed: (".$mysqli->errno.") ".$mysqli->error;
 		}
 	}
 
--- myanimelist.net/mal_history.user.js
+++ myanimelist.net/mal_history.user.js
@@ -1,29 +1,36 @@
 // ==UserScript==
 // @name         MyAnimeList - Infinite History
-// @namespace    https://github.com/DakuTree/various/tree/master/experiments/mal
+// @namespace    https://github.com/DakuTree/userscripts
 // @author       Daku (admin@codeanimu.net)
-// @description  Infinite History
+// @description  MAL history which lasts forever. View on /history/ page.
 // @include      /^http[s]?:\/\/myanimelist\.net\/(anime|manga)(list)?\/.*$/
 // @include      /^http[s]?:\/\/myanimelist\.net\/(anime|manga)\.php\?id\=.*$/
 // @include      /^http[s]?:\/\/myanimelist\.net\/panel\.php\?go\=(edit|add).*$/
 // @include      /^http[s]?:\/\/myanimelist\.net\/editlist\.php\?type\=(anime|manga).*$/
-// @updated      2014-08-24
-// @version      1.0.3
+// @updated      2014-09-28
+// @version      2.0.0 pre-release
 // ==/UserScript==
 
-/* MAL is running jQuery 1.8.1 */
-//TODO: Make work with multiple people
-//TODO: Make work with precise scores (This would also require making precise scores work on the below pages.
-//TODO: Make work on edit popup on list pages.
-//TODO: properly return error if cannot insert
+var backend = "http://codeanimu.net/userscripts/myanimelist.net/backend/";
+var dev = false; //Enable if you want some dev stuff
 
-// var ajax = "http://codeanimu.net/mal/";
-var ajax = "http://192.168.0.5/mal/history.php";
-run_script();
+//TODO: Show AJAX list on history page
 
-function run_script(){
-	var userid = (document.cookie.match('(^|; )Y=([^;]*)')||0)[2];
-	if(userid == 0) return false; //TODO: Alert user?
+$(document).ready(function() {
+	if(dev == true){
+		if(jQuery.fn.jquery !== '1.8.1'){
+			alert('jQuery mismatch!\nRunning '+jQuery.fn.jquery+'.\nExpected 1.8.1.');
+		}
+		window.onerror = function(msg, url, linenumber) {
+			alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+			error = true;
+			return true;
+		}
+	}
+
+	var userid = (document.cookie.match('(^|; )Y=([^;]*)')||0)[2] || 0,
+	    type;
+	if(userid == 0) return false;
 
 	if(/myanimelist\.net\/(anime|manga)\/[0-9]+/.test(self.location.href) || /myanimelist\.net\/(anime|manga)\.php\?id\=.*/.test(self.location.href)){
 		$('[name=myinfo_submit]').click(function(){
@@ -31,8 +38,8 @@ function run_script(){
 				type: self.location.pathname.split('/')[1].split('.')[0], addup: $('[name=myinfo_submit]').val(),
 				db_id: $('[name="aid"], [name="mid"]').attr('value'),
 				title: $('#contentWrapper > h1:first-of-type').contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).text().trim(),
-				status: $('#myinfo_status option:selected').text(),
-				score: $('#myinfo_score option:selected').text().replace( /\D+/g, '') || null,
+				status: $('#myinfo_status option:selected').text().trim(),
+				score: get_score(1, null) || null,
 				//anime
 				ep_watched: $('#myinfo_watchedeps').val() || null,
 				ep_count: $('#curEps').text() || null,
@@ -43,11 +50,10 @@ function run_script(){
 				vol_count: $('#totalVols').text() || null
 			});
 		});
-	}else if(/myanimelist\.net\/(anime|manga)list\/.*/.test(self.location.href)){
-		//TODO: Does this work with the edit popup?
-
+	}
+	else if(/myanimelist\.net\/(anime|manga)list\/.*/.test(self.location.href)){
 		//We can't bind to form submit here since most of these inputs use bind events.
-		$('input').bind("keypress", function(e) {
+		$('input').keypress(function(e) {
 			var key =  e.keyCode || e.which;
 			if (key == 13){
 				handle_input($(this).closest('tr'));
@@ -55,24 +61,26 @@ function run_script(){
 				return true;
 			}
 		});
-		$('input[type="button"]').bind("click", function(e) {
+		$('input[type="button"]').click(function(e) {
 			handle_input($(this).closest('tr'));
 		});
 
-		$('tr a:contains("+")').bind("click", function(e) {
-			var newscore = parseInt($(this).parent().find('span').text()) + 1;
-			if(isNaN(newscore)) newscore = 1;
-			$(this).parent().find('input[type=text]').val(newscore);
+		//+1 episode/chapter/volume
+		$('tr a:contains("+")').click(function(e) {
+			var new_cve = parseInt($(this).parent().find('span').text()) + 1;
+			if(isNaN(new_cve)) new_cve = 1;
+			$(this).parent().find('input[type=text]').val(new_cve);
 			handle_input($(this).closest('tr'));
 		});
-	}else if(/myanimelist\.net\/panel\.php\?go\=(edit|add).*/.test(self.location.href) || /myanimelist\.net\/editlist\.php\?type\=(anime|manga).*/.test(self.location.href)){
+	}
+	else if(/myanimelist\.net\/panel\.php\?go\=(edit|add).*/.test(self.location.href) || /myanimelist\.net\/editlist\.php\?type\=(anime|manga).*/.test(self.location.href)){
 		$('input[type=button][value^=Add], input[type=button][value^=Update]').click(function(){
 			submit_history({
 				type: $('#animeid, #mangaid').attr('id').substr(0, 5), addup: $('#contentWrapper > h1').text().trim(),
 				id: $('#animeid, #mangaid').val(),
 				title: $('strong').text().trim(),
 				status: $('#status option:selected').text().trim(),
-				score: $('[name=score] option:selected').text().replace( /\D+/g, '') || null,
+				score: get_score(3, null) || null,
 				//anime
 				ep_watched: $('#completedEpsID').val() || null,
 				ep_count: $('#totalEpisodes').text() || null,
@@ -84,38 +92,58 @@ function run_script(){
 			});
 		});
 	}
-}
 
-function handle_input(row){
-	submit_history({
-		type: $('#listType').attr('value'), addup: 'Update',
-		db_id: $(row).find('[id^=tagLinks]').attr('id').replace( /\D+/g, ''),
-		title: $(row).find('.animetitle').text(), //This is labeled "animetitle" on both anime & manga lists.
-		status: $('.header_title').text().trim(), //TODO: This may be mismatched with the /anime/ pages, check this.
-		score: $(row).find('input[id^=score]').val() || $(row).find('span[id^=score]').text() || null, //FIXME: Won't work with precise scores..
-		//anime
-		ep_watched: $(row).find('input[id^=epID]').val() || $(row).find('span[id^=output]').text() || null,
-		ep_count: $(row).find('[id^=epText] > a').contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).text().trim().split('/')[1],
-		//manga
-		ch_read: $(row).find('input[id^=chaptext]').val() || $(row).find('span[id^=chap]').text() || null,
-		ch_count: $(row).find('span[id^=chap]').parent().contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).text().trim().split('/')[1],
-		vol_read: $(row).find('input[id^=voltext]').val() || $(row).find('span[id^=vol]').text() || null,
-		vol_count: $(row).find('span[id^=vol]').parent().contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).text().trim().split('/')[1]
-	});
-}
+	function handle_input(row){
+		submit_history({
+			type: $('#listType').attr('value'), addup: 'Update',
+			db_id: $(row).find('[id^=tagLinks]').attr('id').replace( /\D+/g, ''),
+			title: $(row).find('.animetitle').text(), //This is labeled "animetitle" on both anime & manga lists.
+			status: $('.header_title').text().trim(), //TODO: This may be mismatched with the /anime/ pages, check this.
+			score: get_score(2, row) || null, //FIXME: Won't work with precise scores..
+			//anime
+			ep_watched: $(row).find('input[id^=epID]').val() || $(row).find('span[id^=output]').text() || null,
+			ep_count: $(row).find('[id^=epText] > a').contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).text().trim().split('/')[1],
+			//manga
+			ch_read: $(row).find('input[id^=chaptext]').val() || $(row).find('span[id^=chap]').text() || null,
+			ch_count: $(row).find('span[id^=chap]').parent().contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).text().trim().split('/')[1],
+			vol_read: $(row).find('input[id^=voltext]').val() || $(row).find('span[id^=vol]').text() || null,
+			vol_count: $(row).find('span[id^=vol]').parent().contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).text().trim().split('/')[1]
+		});
+	}
 
-function submit_history(info){
-	//To avoid duplicate code, we clean this all here...
-	['score', 'ep_watched', 'ep_count', 'ch_read', 'ch_count', 'vol_read', 'vol_count'].forEach(function(e){
-		info[e] = (info[e] === '-' || info[e] === '?' ? null : info[e]);
-		if(e == 'ep_count' || e == 'ch_read' || e == 'vol_read'){ info[e] = (info[e] == 0 ? null : info[e]); }
-	});
+	function submit_history(info){
+		info['userid'] = userid;
+		//To avoid duplicate code, we clean this all here...
+		['score', 'ep_watched', 'ep_count', 'ch_read', 'ch_count', 'vol_read', 'vol_count'].forEach(function(e){
+			info[e] = (info[e] === '-' || info[e] === '?' ? null : info[e]);
+			if(e == 'ep_count' || e == 'ch_read' || e == 'vol_read'){ info[e] = (info[e] == 0 ? null : info[e]); }
+		});
 
-	console.log("Attempting to update...");
-	$.post(ajax, info, function(data){
-		if(data){
-			console.log("STUFF HAPPENED: "+data);
-			alert(data);
+		console.log("Attempting to update history...");
+		$.post(backend+'mh_update.php', info, function(data){
+			if(data){
+				console.log("ERROR: "+data);
+				if(dev){ alert("ERROR: "+data);	}
+			}
+		});
+	}
+
+	function get_score(type, row){
+		var score = null,
+		    loaded = (this['unsafeWindow'] || window)['loaded_ps'];
+
+		if(type == 1){
+			score = (loaded ? $('#myinfo_score').val()+'.'+$('#precise_score').val() : $('#myinfo_score').val());
 		}
-	});
-}
+		else if(type == 2){
+			score = $(row).find('input[id^=score]').val(); //Set score from input value
+			if(loaded && !score){ score = $(row).find('span[id^=score]').css('content').replace(/('|")/g, ''); }
+			score = (score || $(row).find('span[id^=score]').parent().text());
+		}
+		else if(type == 3){
+			score = (loaded ? $('select[name=score]').val()+'.'+$('#precise_score').val() : $('select[name=score]').val());
+		}
+
+		return score || null;
+	}
+});
