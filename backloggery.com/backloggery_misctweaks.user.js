51e519ca97ba14b435da16ceac7a1f2ae5605120
Daku <admin@codeanimu.net>
2015-01-26 13:34:25 +0000

    userscript for backloggery which sorts games tagged with [on-hold] to bottom of now playing list i've called this misc tweaks incase i come back to this later.

---
+++ backloggery.com/backloggery_misctweaks.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Backloggery - Misc Tweaks
+// @namespace    https://github.com/DakuTree/userscripts
+// @author       Daku (admin@codeanimu.net)
+// @include      /^http[s]?:\/\/(www\.)?backloggery\.com\/.*$/
+// @updated      2015-01-08
+// @version      1.0.0
+// ==/UserScript==
+
+// {
+	var elements = $('.npgame:contains(\[on-hold\])');
+	if(elements){
+		$('#intro').append($('<h1/>', {text: 'On-Hold', style: 'margin-top: 10px;'}));
+		$(elements).each(function(){
+			var a = $(this),
+				b = $(a).next();
+
+			$(a, b).appendTo('#intro');
+		});
+	}
+// }
