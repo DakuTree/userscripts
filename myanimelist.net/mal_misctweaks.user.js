123b5a6b53950f7e7e6667982375fa093ac70ddd
Daku <admin@codeanimu.net>
2015-03-22 16:05:37 +0000

    fix change page type

--- myanimelist.net/mal_misctweaks.user.js
+++ myanimelist.net/mal_misctweaks.user.js
@@ -5,12 +5,9 @@
 // @description  Various small tweaks to MAL
 // @include      /^http[s]?:\/\/myanimelist\.net\/(anime|manga|people|character)(\/|\.php\?id\=).*$/
 // @updated      2014-10-04
-// @version      1.0.0
+// @version      1.0.1
 // ==/UserScript==
 
-var tcheck =  $('#horiznav_nav a:contains("Details")').attr('href').replace(/^.*myanimelist.net\//, '').replace(/^\//, '').split('/'),
-	type   = (tcheck[0] == "anime" ? 0 : (tcheck[0] == "manga" ? 1 : (tcheck[0] == "character" ? 2 : (tcheck[0] == "people" ? 6 : 0)))),
-	id     = tcheck[1];
-
 /* Change search to current page type */
-$('#topSearchValue').val((idtype[1] == "anime" ? 0 : (idtype[1] == "manga" ? 1 : (idtype[1] == "character" ? 2 : (idtype[1] == "people" ? 6 : 0)))));
+var type = location.href.match(/^http[s]?:\/\/myanimelist\.net\/(anime|manga|people|character)(\/|\.php).*$/)[1];
+$('#topSearchValue').val((type == "anime" ? 0 : (type == "manga" ? 1 : (type == "character" ? 2 : (type == "people" ? 6 : 0)))));
