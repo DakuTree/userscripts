f0b3403c33474baeaeecdeaf443f2f65f3119502
Daku <admin@codeanimu.net>
2015-11-17 14:30:56 +0000

    1.1.0 - Note type of series in related block

--- myanimelist.net/mal_misctweaks.user.js
+++ myanimelist.net/mal_misctweaks.user.js
@@ -6,10 +6,18 @@
 // @homepageURL  https://github.com/DakuTree/userscripts
 // @supportURL   https://github.com/DakuTree/userscripts/issues
 // @include      /^http[s]?:\/\/myanimelist\.net\/(anime|manga|people|character)(\/|\.php\?id\=).*$/
-// @updated      2014-10-04
-// @version      1.0.1
+// @updated      2015-11-17
+// @version      1.1.0
 // ==/UserScript==
 
 /* Change search to current page type */
 var type = location.href.match(/^http[s]?:\/\/myanimelist\.net\/(anime|manga|people|character)(\/|\.php).*$/)[1];
 $('#topSearchValue').val((type == "anime" ? 0 : (type == "manga" ? 1 : (type == "character" ? 2 : (type == "people" ? 6 : 0)))));
+
+/* Note type of series in related block */
+//TODO: This needs support for novels
+$('.anime_detail_related_anime a').each(function() {
+	$(this).text(
+		$(this).text() + " ("+$(this).attr('href').match(/(anime|manga)/)[0].replace(/^(.)/, function(e){return e.toUpperCase(); })+")"
+	);
+});
