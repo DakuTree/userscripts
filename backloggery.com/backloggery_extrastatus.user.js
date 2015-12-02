70dab6991447bc08d4c4ef87bae821efb7d43531
Daku <admin@codeanimu.net>
2015-12-02 11:41:15 +0000

    1.2.0 - allow the user to specify their own categories in about this allows for users to create their own categories that are to visible to others with the script

--- backloggery.com/backloggery_extrastatus.user.js
+++ backloggery.com/backloggery_extrastatus.user.js
@@ -2,19 +2,30 @@
 // @name         Backloggery - NP Extra Statuses
 // @namespace    https://github.com/DakuTree/userscripts
 // @author       Daku (admin@codeanimu.net)
-// @description  Adds ability to filter "Now Playing" games under "On-Hold" & "Plan to Play".
+// @description  Adds ability to filter "Now Playing" games under specific categories (on-hold, plan-to-play, etc.)
 // @homepageURL  https://github.com/DakuTree/userscripts
 // @supportURL   https://github.com/DakuTree/userscripts/issues
 // @include      /^http[s]?:\/\/(?:www\.)?backloggery\.com\/(?:.(?!\.php))+$/
-// @updated      2015-05-30
-// @version      1.1.2
+// @updated      2015-12-02
+// @version      1.2.0
 // ==/UserScript==
 
-var categories = {
-	"on-hold": "On-Hold",
-	"to-play": "Plan to Play" //FIXME: This feels like shitty wording.
+var defaultCategories = {
+	//now-playing is used if none is specified.
+	"now-playing" : {
+		"description" : ""
+	}
 };
-
+var defaultCategoriesEnd = {
+	"on-hold" : {
+		"title"       : "On-Hold",
+		"description" : ""
+	},
+	"to-play" : {
+		"title"       : "Plan to Play",
+		"description" : ""
+	}
+}
 var dev = false; //Enable if you want some dev stuff.
 
 $(document).ready(function() {
@@ -41,11 +52,49 @@ $(document).ready(function() {
 		}
 	});
 
-	/** SCRIPT **/
-	$.each(categories, function(key, value) {
+	/**** SCRIPT ****/
+
+	//Grab user settings
+	var userCategoriesStr = $('#note').text().match(/==NPES-START==([\s\S]+)==NPES-END==/);
+	var userCategories    = {};
+	if(userCategoriesStr) {
+		try {
+			userCategories = JSON.parse(userCategoriesStr[1]);
+		} catch (e) {
+			console.error(e);
+		}
+	}
+
+	$.each($.extend(defaultCategories, userCategories, defaultCategoriesEnd), function(key, value) {
+		if(!key.match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]+$/)) {
+			console.warn("User category key contains invalid characters: "+key);
+			return true;
+		} else if(!value['title'].match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]+$/)) {
+			console.warn("User title contains invalid characters: "+value['title']);
+			return true;
+		} else if(!value['description'].match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]*$/)) {
+			console.warn("User description contains invalid characters: "+value['description']);
+			return true;
+		}
+		if(key == 'now-playing' && value['description']) {
+			//the now-playing key is used for anything without a key.
+			$('#intro h1:containsNC("Now Playing")').after(
+				$('<span/>', {class: 'hd-desc', text: value['description']})
+			);
+			$('#intro h1:containsNC("Now Playing")').attr('style', 'margin-bottom:3px');
+			return true;
+		}
+
 		var elements = $('.npgame:containsNC(\['+key+'\])');
 		if(elements.length > 0){
-			$('#intro').append($('<h1/>', {text: value, style: 'margin-top: 12px;'})); //Add header
+			$('#intro').append(
+				$('<h1/>', {text: value['title'], style: 'margin-top: 12px;' + (value['description'] ? ' margin-bottom: 3px;' : '')})
+			).append(
+				(value['description'].length > 0 ?
+					$('<span/>', {class: 'hd-desc', text: value['description']}) :
+					''
+				)
+			);
 
 			//move elements containing key to bottom of list
 			$(elements).each(function(){
@@ -64,4 +113,5 @@ $(document).ready(function() {
 			});
 		}
 	});
+    $('<style/>').attr('rel', 'stylesheet').attr('type', 'text/css').text(".hd-desc {display: block; margin-bottom: 5px;}").appendTo('head');
 });
