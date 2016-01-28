447f3752017ff8f840e57d2668ae0d0934e8e473
Daku <admin@codeanimu.net>
2016-01-28 20:01:05 +0000

    1.0.3 - only create links if user is logged in

--- myanimelist.net/mal_headerlinks.user.js
+++ myanimelist.net/mal_headerlinks.user.js
@@ -9,60 +9,62 @@
 // @exclude      http://myanimelist.net/animelist/*
 // @exclude      http://myanimelist.net/mangalist/*
 // @updated      2016-01-28
-// @version      1.0.2
+// @version      1.0.3
 // ==/UserScript==
 /* jshint -W097 */
 /* global $:false, jQuery:false */
 'use strict';
 
-//Re-add list links.
-var [animeLink, mangaLink] = $('.header-list-button[title=List] + .arrow_box a[href*="list/"]').map(function(){
-	return $(this).attr("href");
-}).get();
+if($('.btn-signup').length === 0) {
+	//Re-add list links.
+	var [animeLink, mangaLink] = $('.header-list-button[title=List] + .arrow_box a[href*="list/"]').map(function(){
+		return $(this).attr("href");
+	}).get();
 
-['Anime', 'Manga'].forEach(function(type) {
-	$('#nav > li:contains("'+type+'") > ul').prepend(
-		$('<li/>').append(
-			$('<a/>', {href: (type == 'Anime' ? animeLink : mangaLink), text: type+' List'})
+	['Anime', 'Manga'].forEach(function(type) {
+		$('#nav > li:contains("'+type+'") > ul').prepend(
+			$('<li/>').append(
+				$('<a/>', {href: (type == 'Anime' ? animeLink : mangaLink), text: type+' List'})
+			)
+		);
+	});
+
+
+	//Re-add profile tab + links
+	var profileLinks = $('.header-profile-link + .arrow_box a[href*="/"]');
+
+	$('#nav').prepend(
+		$('<li/>', {class: 'small'}).append( //<li>
+			$('<a/>', {href: $(profileLinks[0]).attr('href'), /*class: 'non-link',*/ text: 'Profile'})).append(
+			$('<ul/>', {class: 'wider', style: 'display: none;'}).append( //<ul>
+				$('<li/>').append(
+					$(profileLinks[1]) //Friends
+				)
+			).append(
+				$('<li/>').append(
+					$(profileLinks[2]) //Clubs
+				)
+			).append(
+				$('<li/>').append(
+					$(profileLinks[3]) //Blog Posts
+				)
+			).append(
+				$('<li/>').append(
+					$(profileLinks[4]) //Reviews
+				)
+			).append(
+				$('<li/>').append(
+					$(profileLinks[5]) //Reccomendations
+				)
+			).append(
+				$('<li/>').append(
+					$(profileLinks[6]) //Account Settings
+				)
+			)
 		)
 	);
-});
-
-
-//Re-add profile tab + links
-var profileLinks = $('.header-profile-link + .arrow_box a[href*="/"]');
-
-$('#nav').prepend(
-	$('<li/>', {class: 'small'}).append( //<li>
-		$('<a/>', {href: $(profileLinks[0]).attr('href'), /*class: 'non-link',*/ text: 'Profile'})).append(
-		$('<ul/>', {class: 'wider', style: 'display: none;'}).append( //<ul>
-			$('<li/>').append(
-				$(profileLinks[1]) //Friends
-			)
-		).append(
-			$('<li/>').append(
-				$(profileLinks[2]) //Clubs
-			)
-		).append(
-			$('<li/>').append(
-				$(profileLinks[3]) //Blog Posts
-			)
-		).append(
-			$('<li/>').append(
-				$(profileLinks[4]) //Reviews
-			)
-		).append(
-			$('<li/>').append(
-				$(profileLinks[5]) //Reccomendations
-			)
-		).append(
-			$('<li/>').append(
-				$(profileLinks[6]) //Account Settings
-			)
-		)
-	)
-);
-$('#nav li:contains("Profile")').hover(function() {
-	$(this).toggleClass('hover');
-	$(this).find('> ul').toggle();
-});
+	$('#nav li:contains("Profile")').hover(function() {
+		$(this).toggleClass('hover');
+		$(this).find('> ul').toggle();
+	});
+}
