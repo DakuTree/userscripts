c855187c35325911c4c6c561133c6e1350e9bc71
Daku <admin@codeanimu.net>
2017-09-09 21:13:25 +0100

    New WIP userscript! - "HorribleSubs - Filter Series" Very WIP. Requires manually editting the userscript to add new stubs, and only triggers on refresh.

---
+++ horriblesubs.info/filter.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         HorribleSubs - Filter Series
+// @namespace    https://github.com/DakuTree/userscripts
+// @author       Daku (admin@codeanimu.net)
+// @description  Allows you to filter series on the HorribleSubs release list.
+// @homepageURL  https://github.com/DakuTree/userscripts
+// @supportURL   https://github.com/DakuTree/userscripts/issues
+// @include      /^https?:\/\/horriblesubs\.info($|\/.*$)$/
+// @updated      2017-09-09
+// @version      1.0.0
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @grant        GM_addStyle
+// ==/UserScript==
+/* jshint -W097, browser:true, devel:true */
+/* global $:false, jQuery:false, add_ep_click_event, GM_addStyle */
+'use strict';
+
+const filtered = [
+	'hitorijime-my-hero',
+	'detective-conan',
+	'hina-logi-from-luck-logic',
+	'bonobono',
+	'100-pascal-sensei',
+	'pripri-chii-chan',
+	'action-heroine-cheer-fruits',
+	'nobunaga-no-shinobi'
+];
+
+$(function() {
+	GM_addStyle(".filtered {display: none;}");
+
+	//Add filter toggle
+	$('#search').prepend(
+		$('<div/>', {id: 'filter-container', style: 'display: inline-block; font-weight: 400; position: absolute; left: 0; top: 6px;'}).append(
+			$('<input/>', {type: 'checkbox', id: 'filter'}).change(function() {
+				if(this.checked) {
+					$('.filtered').addClass('filtered-show').removeClass('filtered');
+				} else {
+					$('.filtered-show').removeClass('filtered-show').addClass('filtered');
+				}
+			})
+		).append(
+			$('<label/>', {text: 'Show Filtered', style: 'vertical-align: bottom;'})
+		)
+	);
+
+	//Refresh link.
+	$('.refreshlink').replaceWith($('.refreshlink').clone()); //Reset events, since .off() doesn't appear to work here.
+	$('.refreshlink').on('click', '.refreshbutton', function(e) {
+		e.preventDefault();
+
+        $.ajax({
+            url: '/lib/latest.php',
+            success: function(response) {
+                $('.latest').fadeOut(function () {
+					let html       = $(response);
+
+					html.filter('.release-info').each(function(i, e) {
+						let ele    = $(e),
+						    stub   = ele.find('a[title="See all releases for this show"]').attr('href').substr(7)/*,
+						    extras = ele.nextUntil('table')*/;
+						if($.inArray(stub, filtered) !== -1) {
+							console.log(`Stub matched (${stub})`);
+							ele.addClass('filtered');
+							// html.splice(html.index(e), 1 + extras.length);
+						}
+					});
+
+                    $('.latest').html(html).fadeIn();
+
+                    add_ep_click_event();
+                });
+                $('.morebox').html('<a href="#" style="display: block;" class="morebutton" id="0">Show more</a>');
+                $('.refreshlink a').html('<i class="dashicons dashicons-update" title="Refresh"></i>');
+            }
+        });
+    });
+	$('refreshbutton').click(); //FIXME: We should just handle the original load event on our own.
+});
