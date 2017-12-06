e6675183199d16e0e8f3a5dd160a4e4326ad70d2
Angus Johnston <dakutree@codeanimu.net>
2017-12-06 19:49:23 +0000

    2.0.0 - Script rewrite + added priority sorting (via [p:N] tag)

--- backloggery.com/backloggery_extrastatus.user.js
+++ backloggery.com/backloggery_extrastatus.user.js
@@ -2,48 +2,43 @@
 // @name         Backloggery - NP Extra Statuses
 // @namespace    https://github.com/DakuTree/userscripts
 // @author       Daku (admin@codeanimu.net)
-// @description  Adds ability to filter "Now Playing" games under specific categories (on-hold, plan-to-play, etc.)
+// @description  Adds ability to filter "Now Playing" games under specific categories (on-hold, plan-to-play, etc.) & order them.
 // @homepageURL  https://github.com/DakuTree/userscripts
 // @supportURL   https://github.com/DakuTree/userscripts/issues
 // @include      /^http[s]?:\/\/(?:www\.)?backloggery\.com\/(?:.(?!\.php))+$/
-// @updated      2016-03-09
-// @version      1.2.1
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @updated      2017-12-XX
+// @version      2.0.0
+// @grant        GM_addStyle
 // ==/UserScript==
 
-var defaultCategories = {
-	//now-playing is used if none is specified.
-	"now-playing" : {
-		"description" : ""
-	}
-};
-var defaultCategoriesEnd = {
-	"on-hold" : {
-		"title"       : "On-Hold",
-		"description" : ""
+/*** OPTIONS ***/
+const defaultCategories = {
+	"start" : {
+		//now-playing is used if none is specified.
+		"now-playing" : {
+			"description" : ""
+		}
 	},
-	"to-play" : {
-		"title"       : "Plan to Play",
-		"description" : ""
-	}
-}
-var dev = false; //Enable if you want some dev stuff.
-
-$(document).ready(function() {
-	if(dev == true){
-		if(jQuery.fn.jquery !== '1.8.3') alert('jQuery mismatch!\nRunning '+jQuery.fn.jquery+'.\nExpected 1.8.3.');
-
-		//https://danlimerick.wordpress.com/2014/01/18/how-to-catch-javascript-errors-with-window-onerror-even-on-chrome-and-firefox/
-		window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
-			alert('Error: '+errorMsg+'\n'
-			     +'ScriptURL: '+url+'\n'
-			     +'LineNumber: '+lineNumber+'\n'
-			     +'Column: '+column+'\n'
-			     +'StackTrace: '+errorObj);
-
-			error = true;
-			return true;
+	"end"   : {
+		"on-hold" : {
+			"title"       : "On-Hold",
+			"description" : ""
+		},
+		"to-play" : {
+			"title"       : "Plan to Play",
+			"description" : ""
 		}
 	}
+};
+
+$(function() {
+	GM_addStyle(`
+		.hd-desc {
+			display: block;
+			margin-bottom: 5px;
+		}
+	`);
 
 	//Case insensitive :contains || via: https://gist.github.com/jbcappell/2648373
 	$.extend($.expr[":"], {
@@ -51,116 +46,151 @@ $(document).ready(function() {
 			return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
 		}
 	});
+	//https://github.com/padolsey-archive/jquery.fn/blob/master/sortElements/jquery.sortElements.js
+	jQuery.fn.sortElements=(function(){var sort=[].sort;return function(comparator,getSortable){getSortable=getSortable||function(){return this;};var placements=this.map(function(){var sortElement=getSortable.call(this),parentNode=sortElement.parentNode,nextSibling=parentNode.insertBefore(document.createTextNode(''),sortElement.nextSibling);return function(){if(parentNode===this){throw new Error("You can't sort elements if any one is a descendant of another.");} parentNode.insertBefore(this,nextSibling);parentNode.removeChild(nextSibling);};});return sort.call(this,comparator).each(function(i){placements[i].call(getSortable.call(this));});};})();
 
 	/**** SCRIPT ****/
+	let userCategories = getCategories();
+	$.each($.extend(defaultCategories.start, userCategories, defaultCategories.end), function(tag, data) {
+		let cateTitle       = data.title,
+		    cateDescription = data.description;
 
-	//Grab user settings
-	var userCategoriesStr = $('#note').text().match(/==NPES-START==([\s\S]+)==NPES-END==/);
-	var userCategories    = {};
-	if(userCategoriesStr) {
-		try {
-			userCategories = JSON.parse(userCategoriesStr[1]);
-		} catch (e) {
-			console.error(e);
-		}
-	}
-
-	$.each($.extend(defaultCategories, userCategories, defaultCategoriesEnd), function(key, value) {
-		if(!key.match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]+$/)) {
-			console.warn("User category key contains invalid characters: "+key);
+		//Validate data.
+		if(!tag.match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]+$/)) {
+			console.warn(`User category tag contains invalid characters: ${tag}`);
 			return true;
-		} else if(!value['title'].match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]+$/)) {
-			console.warn("User title contains invalid characters: "+value['title']);
+		} else if(!cateTitle.match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]+$/)) {
+			console.warn(`User title contains invalid characters: ${cateTitle}`);
 			return true;
-		} else if(!value['description'].match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]*$/)) {
-			console.warn("User description contains invalid characters: "+value['description']);
+		} else if(!cateDescription.match(/^[a-zA-Z0-9-_\(\)\.\?\#\~\:\{\}\"\'\, ]*$/)) {
+			console.warn(`User description contains invalid characters: ${cateDescription}`);
 			return true;
 		}
-		if(key == 'now-playing' && value['description']) {
-			//the now-playing key is used for anything without a key.
-			$('#intro h1:containsNC("Now Playing")').after(
-				$('<span/>', {class: 'hd-desc', text: value['description']})
+
+		//The now-playing already exists, however the user can add a description which we need to add to account for.
+		if(tag === 'now-playing' && cateDescription) {
+			let header = $('#intro h1:containsNC("Now Playing")');
+			header.after(
+				$('<span/>', {class: 'hd-desc', text: cateDescription})
 			);
-			$('#intro h1:containsNC("Now Playing")').attr('style', 'margin-bottom:3px');
+			header.attr('style', 'margin-bottom: 3px');
+
 			return true;
 		}
 
-		var elements = $('.npgame:containsNC(\['+key+'\])');
-		if(elements.length > 0){
-			$('#intro').append(
-				$('<h1/>', {text: value['title'], style: 'margin-top: 12px;' + (value['description'] ? ' margin-bottom: 3px;' : '')})
-			).append(
-				(value['description'].length > 0 ?
-					$('<span/>', {class: 'hd-desc', text: value['description']}) :
-					''
-				)
+		//Check game list for series containing category.
+		let games = $(`.npgame:containsNC([${tag}])`);
+		if(games.length > 0){
+			let intro = $('#intro');
+			intro.append(
+				$('<h1/>', {text: cateTitle, style: 'margin-top: 12px;'})
 			);
+			if(cateDescription.length > 0) {
+				intro.append(
+					$('<span/>', {class: 'hd-desc', text: cateDescription, style: 'margin-top: 3px;'})
+				);
+			}
 
-			//move elements containing key to bottom of list
-			var div = $('<div/>', {class: 'es-games'});
-			$(elements).each(function(){
-				var a = $(this);
+			let games_container = $('<div/>', {class: 'es-games'});
+			$(games).each(function(){
+				let game            = $(this),
+					gameDescription = game.children(':eq(4)').text().trim(),
+				    priority        = 0;
 
-				//remove the tag from the description if at start or end. keep otherwise (to avoid problems)
-				$(a).children(':eq(4)').text(function() {
-					$(this).parent().attr('data-text', $(this).text().trim());
+				game.parent().attr('data-text', gameDescription); //Keep the original description for editting purposes.
+
+
+				//Check if game also has priority
+				let pArr = gameDescription.match(/\[p\:([0-9]+)]/);
+				if(pArr) {
+					priority = parseInt(pArr[1]);
+				}
+				game.attr('data-priority', priority);
+				//TODO: We should also add some form of icon.
+
+				//Remove tags from visible description.
+				$(game).children(':eq(4)').find('span').text(function() {
 					//Check: Is there a better way to do this regex? Lots of duplication here..
-					var re = new RegExp('(?:^[\\s]*\\['+key+'\\][\\s]*|[\\s]*\\['+key+'\\][\\s]*$)','i');
+					let re = new RegExp('(?:^[\\s]*\\['+tag+'\\][\\s]*|[\\s]*\\['+tag+'\\][\\s]*$)','i');
+					//TODO: Remove priority after we add prio icon.
+					// var re = new RegExp('(?:^[\\s]*\\[p\\:[0-9]+\\][\\s]*|[\\s]*\\[p\\:[0-9]+\\][\\s]*$)','i');
 					return $(this).text().replace(re, '');
 				});
 
-				var b = $(a).next();
+				let game_info = $(game).next();
+				let game_container = $('<div/>', {class: 'es-game'})
+					.append(game)
+					.append(game_info); //Backloggery stores the "more" data in the next div, which lacks proper IDs.
 
-
-				var g_div = $('<div/>', {class: 'es-game'});
-
-				$(a).appendTo(g_div);
-				$(b).appendTo(g_div);
-
-				$(g_div).appendTo(div);
+				$(game_container).appendTo(games_container);
 			});
-			$(div).appendTo('#intro');
+			$(games_container).appendTo('#intro');
 		}
 	});
 
 	//Stick non-tagged games in divs.
-	var div = $('<div/>', {class: 'es-games'});
+	let games_container = $('<div/>', {class: 'es-games'});
 	$('#intro > .npgame').each(function(){
-		var a = $(this);
-		var b = $(a).next();
+		let game = $(this);
 
+		let game_info = $(game).next();
+		let game_container = $('<div/>', {class: 'es-game'})
+			.append(game)
+			.append(game_info); //Backloggery stores the "more" data in the next div, which lacks proper IDs.
 
-		var g_div = $('<div/>', {class: 'es-game'});
-
-		$(a).appendTo(g_div);
-		$(b).appendTo(g_div);
-
-		$(g_div).appendTo(div);
+		$(game_container).appendTo(games_container);
 	});
-	$(div).insertAfter($('#intro > h1:first-of-type + .hd-desc') || $('#intro > h1:first-of-type'));
+	$(games_container).insertAfter($('#intro > h1:first-of-type + .hd-desc') || $('#intro > h1:first-of-type'));
 
-	{
-		jQuery.fn.sortElements=(function(){var sort=[].sort;return function(comparator,getSortable){getSortable=getSortable||function(){return this;};var placements=this.map(function(){var sortElement=getSortable.call(this),parentNode=sortElement.parentNode,nextSibling=parentNode.insertBefore(document.createTextNode(''),sortElement.nextSibling);return function(){if(parentNode===this){throw new Error("You can't sort elements if any one is a descendant of another.");}
-		parentNode.insertBefore(this,nextSibling);parentNode.removeChild(nextSibling);};});return sort.call(this,comparator).each(function(i){placements[i].call(getSortable.call(this));});};})();
+	sortGames();
+	
+	/*****************************************/
 
+	function getCategories() {
+		let categoryMatch = $('#note').text().match(/==NPES-START==([\s\S]+)==NPES-END==/),
+		    categories     = {};
+		if(categoryMatch) {
+			try {
+				categories = JSON.parse(categoryMatch[1]);
+			} catch (e) {
+				console.error(e);
+			}
+		}
+		return categories;
+	}
+
+	function sortGames() {
 		$('.es-games').each(function() {
-			$(this).find('.es-game').sortElements(function(a,b) {
-				var a_v = $(a).find('> .npgame > div:eq(2)').text().trim();
-				var b_v = $(b).find('> .npgame > div:eq(2)').text().trim();
+			$(this).find('.es-game').sortElements(function(a, b) {
+				let a_game = $(a).find('> .npgame'),
+				    b_game = $(b).find('> .npgame');
 
-				if(a_v > b_v) {
-					return 1;
-				} else if(a_v < b_v) {
+				let a_platform = a_game.find('> div:eq(2)').text().trim(),
+				    b_platform = b_game.find('> div:eq(2)').text().trim(),
+					a_priority = a_game.attr('data-priority'),
+					b_priority = b_game.attr('data-priority');
+
+				//Sort by priority first
+				if(a_priority > b_priority) {
 					return -1;
+				} else if(a_priority < b_priority) {
+					return 1;
 				} else {
-					var a_v2 = $(a).find('> .npgame > div:eq(3)').text().trim();
-					var b_v2 = $(b).find('> .npgame > div:eq(3)').text().trim();
+					//Then sort by platform.
+					if(a_platform > a_platform) {
+						return 1;
+					} else if(a_platform < b_platform) {
+						return -1;
+					} else {
+						//Then sort by title.
+						let a_title = $(a_game).find('> div:eq(3)').text().trim(),
+						    b_title = $(b_game).find('> div:eq(3)').text().trim();
 
-					return (a_v2 > b_v2) ? 1 : (a_v2 < b_v2) ? -1 : 0;
+						return (a_title > b_title) ? 1 : (a_title < b_title) ? -1 : 0;
+					}
 				}
+
 			});
 		});
 	}
-
-	$('<style/>').attr('rel', 'stylesheet').attr('type', 'text/css').text(".hd-desc {display: block; margin-bottom: 5px;}").appendTo('head');
 });
